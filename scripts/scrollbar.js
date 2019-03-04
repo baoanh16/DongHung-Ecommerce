014395987d28d70932e5f5bdefdac969812732e6
ps06347 <vanhoangbao@gmail.com>
2019-03-04 13:03:31 +0700

    Initial commit

---
+++ scripts/scrollbar.js
@@ -0,0 +1,16 @@
+const main = Scrollbar.init(app.select('.scroll-Animate'), {
+	damping: 0.11,
+	thumbMinSize: 36,
+	renderByPixels: false,
+	alwaysShowTracks: false,
+	continuousScrolling: false
+})
+if (window.innerWidth < 992) {
+	const headerMainNav = Scrollbar.init(app.select('.dh-header .main-nav'), {
+		damping: 0.11,
+		thumbMinSize: 36,
+		renderByPixels: false,
+		alwaysShowTracks: false,
+		continuousScrolling: false
+	})
+}
